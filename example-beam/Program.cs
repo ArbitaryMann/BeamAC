6c3de3c8f9a9da38f0a2e508b5ab9d6bf616a20b
ArbitaryMann <80572669+ArbitaryMann@users.noreply.github.com>
2022-02-14 13:58:45 +0300

    example project added

---
+++ example-beam/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Love;
+using BeamAnticheat;
+namespace anticheat_love
+{
+    public class Game : Scene
+    {
+        BeamAnticheat.ZLab.Lib.Anticheat Lib = new BeamAnticheat.ZLab.Lib.Anticheat();
+        BeamAnticheat.ZLab.Lib LibInfo = new BeamAnticheat.ZLab.Lib();
+      
+
+    static void Main(string[] args)
+        {
+            BeamAnticheat.ZLab.Lib LibInfo = new BeamAnticheat.ZLab.Lib();
+            LibInfo.LogInfo();
+            Boot.Init();
+            Boot.Run(new Game());
+        }
+
+        public override void Draw()
+        {
+        
+            Lib.LoadBeamLib(); /*/ For Attachments of Cheat Engine Debuggers / SpeedHack /*/
+            Graphics.Print("Game Test", 400, 300);
+        }
+    }
+
+}
+
